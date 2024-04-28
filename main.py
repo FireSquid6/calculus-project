fb467d96361ec95b8f8d4887be86995ae0dfc4ae
firesquid6 <jdeiss06@gmail.com>
2024-04-28 13:08:28 -0500

    add the project

---
+++ main.py
@@ -0,0 +1,29 @@
+from manim import *
+from math import *
+
+# class CreateCircle(Scene):
+#     def construct(self):
+#         circle = Circle()  # create a circle
+#         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+#         self.play(Create(circle))  # show the circle on screen
+
+def f(x):
+    return pow(e, x) - 1
+
+def g(x):
+    return 2 * pow(e, -x) + 1
+
+class GraphFunctions(Scene):
+    def construct(self):
+        ax = Axes(
+            x_range=[-2, 6],
+            y_range=[-2, 6],
+            axis_config={"include_numbers": True}
+        )
+
+        f_graph = ax.plot(f, color=BLUE)
+        g_graph = ax.plot(g, color=RED)
+
+        self.add(ax, f_graph, g_graph)
+
+
---
+++ shell.nix
@@ -0,0 +1,10 @@
+{ nixpkgs ? import <nixpkgs> { } }:
+with nixpkgs; mkShell {
+  buildInputs = [
+    nodejs_20
+    python3
+    libgcc
+    poetry
+    manim
+  ];
+}
